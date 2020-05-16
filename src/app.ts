911f624b432fc571d92a0e2b1601354990835497
Juan Billasante <juan.billasante@registrocivilctes.gov.ar>
2020-05-16 10:44:08 -0300

    config socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 
 import express = require('express');
 import { urlencoded } from 'body-parser';
+import socketIO = require('socket.io');
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app: express.Application = express();
 const PORT = process.env.PORT || 5000;
 const morgan = require ('morgan');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-var socket = require('socket.io');
+/* const urlencodedParser = bodyParser.urlencoded({ extended: false }) */
+const socket = require('socket.io');
+const http = require('http');
+
+
 
 app.set('views', 'views');
 app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({ extended: false }))
+/* app.use(bodyParser.urlencoded({ extended: false })) */
 app.use(bodyParser.json())
 app.use(morgan('dev'));
 app.use(express.static('public'));
@@ -23,8 +27,8 @@ var server = app.listen(PORT, function () {
 
 var io = socket(server);
 
-io.on('coneccition',function(socket){
-  console.log('Socket conected')
+io.on('connection', (socket:socketIO.Socket)=>{
+  console.log('Socket connected')
 })
 
 app.get('/', function (_req, _res) {
