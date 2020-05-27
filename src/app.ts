9db80177d80e630a7b89769e066b4df520ebebd1
Juan Billasante <juan.billasante@registrocivilctes.gov.ar>
2020-05-27 19:54:36 -0300

    static

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,18 @@
 
 import express = require('express');
 import socketIO = require('socket.io');
-import path = require('path');
+
 
 
 const app: express.Application = express();
 const PORT = process.env.PORT || 5000;
 const morgan = require ('morgan');
 const socket = require('socket.io');
-const http = require('http');
 
 
-/* app.use(morgan('dev')); */
+/* app.use(morgan('dev'));  */
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
 
 var server = app.listen(PORT, function () {
   console.log('Server on port '+PORT);
